d0e08b809a48b86cc76bf6ec4d77aa1a45d92007
Ivan Tsai <oscar60310@gmail.com>
2017-09-17 01:31:57 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
 echo "START"
-cd dist
-echo "OK"
-git init
-git config user.name "ivan tsai"
-git config user.email "oscar60310@gmail.com"
-git add .
-git commit -m "Deploy commit $ID to github page $DATE"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
