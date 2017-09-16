d0a89a4d3ffccf744a9dc071f496ca57d1269dbe
Ivan Tsai <oscar60310@gmail.com>
2017-09-17 01:07:59 +0800

    auto upload

---
+++ deplay.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd dist
+git init
+git config user.name "ivan tsai"
+git config user.email "oscar60310@gmail.com"
+git add .
+git commit -m "Deploy commit $ID to github page $DATE"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
