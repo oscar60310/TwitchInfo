63e37fa716e052ae8bdd6cdad66e737f135b96de
Ivan Tsai <oscar60310@gmail.com>
2017-09-17 23:40:42 +0800

    test yahoo api

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -7,7 +7,7 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
   getWeather(key: string) {
     return new Promise<any>((resolve, reject) => {
-      this.http.get(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001`, {
+      this.http.get(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`, {
         headers: new HttpHeaders().set('Authorization', key)
       }).subscribe(d => {
         if (d['success']) {
